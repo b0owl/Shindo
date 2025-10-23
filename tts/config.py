eb240581c74443b6787cb9758b277af7da43c28c
Brennan MR <brennanmarxrennie@gmail.com>
2025-10-22 20:47:45 -0400

    fix

--- tts/config.py
+++ tts/config.py
@@ -2,7 +2,7 @@ import pygame
 running = True
 pygame.init()
 # title screen
-font = pygame.font.Font("tts\\_font.ttf", 100)
+font = pygame.font.Font("tts/_font.ttf", 100)
 savesMargin = 70
 settingsMargin = 70
 showTitle = True
